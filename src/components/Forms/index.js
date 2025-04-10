42e27c799f634e860cf33d9faec7d8e1ec22f1de
orafamoura <raphaelmvb2@gmail.com>
2025-04-10 15:17:03 -0300

    useState, onChange e evento.target.value

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Botao from '../Botao/Botao'
 import FormsText from '../FormsText'
 import ListaSuspensa from '../ListaSuspensa'
@@ -15,20 +16,54 @@ const Forms = () => {
         'inovacao e gestao'
     ]
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+    
     const aoSalvar = (evento) => {
         evento.preventDefault() //previne o comportamento padrao da pagina, no caso aqui esta sendo usado para a pagina nao atualizar quando clicar no botao
-        console.log('form foi submetido')
+        console.log('form foi submetido', nome, cargo, imagem, time)
     }
 
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <FormsText obrigatorio={true} label="Nome" placeholder="digite seu nome" />
-                <FormsText obrigatorio={true} label="Cargo" placeholder="digite seu cargo" />
-                <FormsText obrigatorio={true} label="Imagem" placeholder="digite o endereco da imagem" />
-                <ListaSuspensa obrigatorio={true} label="Times" itens={times}/>
-                <Botao texto="Criar Card"/>
+                <FormsText 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <FormsText 
+                    obrigatorio={true} 
+                    label="Cargo" 
+                    placeholder="digite seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+
+                <FormsText 
+                    obrigatorio={true} 
+                    label="Imagem" 
+                    placeholder="digite o endereco da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+
+                <ListaSuspensa 
+                    obrigatorio={true} 
+                    label="Times" 
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                />
+                <Botao 
+                    texto="Criar Card"
+                />
             </form>
         </section>
     )
